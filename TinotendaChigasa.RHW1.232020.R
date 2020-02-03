07e1d63586c350af2d53fa4cea6da2675680bb49
ttchiga <46941822+ttchiga@users.noreply.github.com>
2020-02-03 13:18:55 -0500

    Add files via upload

---
+++ TinotendaChigasa.RHW1.232020.R
@@ -0,0 +1,15 @@
+2017^(1/3)
+abs(5.7-6.8)/.58
+a <- 1:12
+a
+b <- c(1,3,5,7,9,11)# or seq(1,11,2)
+b
+c <- seq(1,11,2)
+c
+ln.a=log(a)
+ln.a
+c^2
+Name='Tinotenda'
+paste('My name is', Name)
+?sd # the function is used to calcuate the standard
+#deviation within a data set
